8ed767557f2e40afff8785901978c8e686059a69
Matt Trifilo <matt.trifilo@gmail.com>
2017-05-12 13:03:41 -0500

    add postcss

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-cssnext': {
+      browsers: ['last 2 versions', '> 5%']
+    },
+    'postcss-reporter': {}
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -41,18 +41,13 @@ module.exports = function () {
           loader: 'json-loader'
         },
         {
-          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'url-loader?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]'
-        },
-        {
-          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'file-loader?name=fonts/[name].[ext]'
-        },
-        {
           test: /\.css$/,
           use: ExtractTextPlugin.extract({
             fallback: 'style-loader',
-            use: ['css-loader']
+            use: [
+              { loader: 'css-loader', options: { importLoaders: 1 } },
+              'postcss-loader'
+            ]
           })
         }
       ]
